c9f4c91b43cda66be543ea1fd30a13e54d7092ea
Fahad Naeem <fahadnaeem838@gmail.com>
2023-04-10 22:41:29 +0500

    modify js

--- index.js
+++ index.js
@@ -12,19 +12,19 @@
 //   console.log(Anonymous2()); 
 
 //QUESTION 2
-// let array = [1, 2, 3, 4, 5, 6, 7];
-// function recursiveFunction(array, valueToBeCheck) {
-//     if (array.length === 0) {
-//       return false;
-//     }
+let array = [1, 2, 3, 4, 5, 6, 7];
+function recursiveFunction(array, valueToBeCheck) {
+    if (array.length === 0) {
+      return false;
+    }
   
-//     else if (array[0] === valueToBeCheck) {
-//       return true;
-//     }
+    else if (array[0] === valueToBeCheck) {
+      return true;
+    }
   
-//     return recursiveFunction(array.slice(1),valueToBeCheck);
-//   }
-//   console.log(recursiveFunction(array, 1)); // Output: true
-//   console.log(recursiveFunction(array, 10)); // Output: false
+    return recursiveFunction(array.slice(1),valueToBeCheck);
+  }
+  console.log(recursiveFunction(array, 1)); // Output: true
+  console.log(recursiveFunction(array, 10)); // Output: false
     
 //QUESTION 3
